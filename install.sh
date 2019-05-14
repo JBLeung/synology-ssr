423d42831197ce0bc35e4464e2fad1b0965b519b
JBLeung <JBLeung@users.noreply.github.com>
2019-05-15 01:13:56 +0800

    Update install.sh
    fix cannot download config issue

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ sudo python get-pip.py;
 ## install ssr
 sudo pip --no-cache-dir install https://github.com/JBLeung/shadowsocksr/archive/manyuser-hide-pw.zip;
 ## get ssr resource for synology
-wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/config.json;
-wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/util.py;
+sudo wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/config.json;
+sudo wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/util.py;
 ## replace update util for python crypto libraray
 sudo mv /usr/lib/python2.7/ctypes/util.py /usr/lib/python2.7/ctypes/bk_util.py;
 sudo mv ./util.py /usr/lib/python2.7/ctypes/util.py;
