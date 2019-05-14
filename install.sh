edc7f9c8744320768eb84d52b596cea1c452f294
JBLeung <JBLeung@users.noreply.github.com>
2019-05-15 01:05:39 +0800

    Update install.sh
    fix comment

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-// create temp dir
+## create temp dir
 mkdir ssr;
 cd ssr;
-// down pip
+## down pip
 wget https://bootstrap.pypa.io/get-pip.py;
 sudo python get-pip.py;
-// install ssr
+## install ssr
 sudo pip --no-cache-dir install https://github.com/JBLeung/shadowsocksr/archive/manyuser-hide-pw.zip;
-// get ssr resource for synology
+## get ssr resource for synology
 wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/config.json;
 wget https://raw.githubusercontent.com/JBLeung/synology-ssr/master/res/util.py;
-// replace update util for python crypto libraray
+## replace update util for python crypto libraray
 sudo mv /usr/lib/python2.7/ctypes/util.py /usr/lib/python2.7/ctypes/bk_util.py;
 sudo mv ./util.py /usr/lib/python2.7/ctypes/util.py;
-// install make
+## install make
 sudo pip install py-make
-// clear
+## clear
